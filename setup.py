1489455e038c83c806f0abe0dc2af4b0a13337c8
Alberto Degli Esposti <ade8850@gmail.com>
2020-01-15 17:44:11 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='krules-cloudstorage',
+    version="0.0.1",
+    author="Alberto Degli Esposti",
+    author_email="alberto@arispot.tech",
+    description="KRules Python cloudstorage support package",
+    licence="Apache Licence 2.0",
+    keywords="krules rules engine gcp cloudstorage bucket",
+    url="...",  #TODO
+    packages=find_packages(), #['krules_core'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: KRules",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=[
+        'google-cloud-storage==1.20.0',
+        'cloudstorage==0.10.0',
+    ],
+    setup_requires=[
+        'pytest-runner',
+
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
