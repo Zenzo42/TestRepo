c1ccf2d145e04f812624df96173af2f87126f4fa
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-05-15 16:18:58 +0200

    add AUTHORS LICENSE and fix setup

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ def read(fname):
 
 setup(
     name='krules-cloudstorage',
-    version="0.0.1",
+    version="0.2.0",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules Python cloudstorage support package",
     licence="Apache Licence 2.0",
     keywords="krules rules engine gcp cloudstorage bucket",
-    url="...",  #TODO
-    packages=find_packages(), #['krules_core'],
+    url="https://github.com/airspot-dev/krules-cloudstorage",
+    packages=find_packages(),
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Framework :: KRules",
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
         'google-cloud-storage==1.20.0',
         'cloudstorage==0.10.0',
+        'krules-core==0.2.0'
     ],
     setup_requires=[
         'pytest-runner',
