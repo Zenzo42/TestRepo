7e10da743c9cab8885bbea3e9539e686be1172fc
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-09-15 11:47:12 +0200

    update core version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-cloudstorage',
-    version="0.4.2",
+    version="0.4.3",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules Python cloudstorage support package",
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'google-cloud-storage==1.20.0',
         'cloudstorage==0.10.0',
-        'krules-core==0.4.2'
+        'krules-core==0.4.3'
     ],
     setup_requires=[
         'pytest-runner',
